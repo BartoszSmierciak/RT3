8e6d7824e832b3185f26b287771f5edd365115c8
BartoszSmierciak <bartosz.smierciak@gmail.com>
2021-06-05 19:12:17 +0200

    Update encoder.h
    changing SET names

--- C/src/encoder.h
+++ C/src/encoder.h
@@ -35,7 +35,7 @@ uint32_t EncoderGetPosition(int slaveAddress);
 uint32_t EncoderGetActualReverseState(int slaveAddress);
 
 //8 bit MSB
-uint32_t EncoderGetTermResetState();
+uint32_t EncoderGetTermReEncoderSetState();
 
 //2*32 bit MSB 0xFF00 LSB 0x00FF
 uint32_t EncoderGetSpeed(int slaveAddress);
@@ -56,73 +56,73 @@ uint32_t EncoderGetPhysicalMTResolution(int slaveAddress);
 uint32_t EncoderGetScalingEnabled(int slaveAddress);
 
 /*!
-* Set scaling enabled.
+* EncoderSet scaling enabled.
 * \param slaveAddress
 * \param scalingEnabled
 */
-void SetScalingEnabled(int slaveAddress, uint32_t scalingEnabled);
+void EncoderSetScalingEnabled(int slaveAddress, uint32_t scalingEnabled);
 
 uint32_t EncoderGetSTResolution(int slaveAddress);
 
-void SetSTResolution(int slaveAddress, uint32_t sTResolution);
+void EncoderSetSTResolution(int slaveAddress, uint32_t sTResolution);
 
 uint32_t EncoderGetTotResolution(int slaveAddress);
 
-void SetTotResolution(int slaveAddress, uint32_t totResolution);
+void EncoderSetTotResolution(int slaveAddress, uint32_t totResolution);
 
-uint32_t EncoderGetPreset(int slaveAddress);
+uint32_t EncoderGetPreEncoderSet(int slaveAddress);
 
-void SetPreset(int slaveAddress, uint32_t preset);
+void EncoderSetPreEncoderSet(int slaveAddress, uint32_t preEncoderSet);
 
-uint32_t EncoderGetOffset(int slaveAddress);
+uint32_t EncoderGetOffEncoderSet(int slaveAddress);
 
 uint32_t EncoderGetCountDirection(int slaveAddress);
 
-void SetCountDirection(int slaveAddress, uint32_t countDirection);
+void EncoderSetCountDirection(int slaveAddress, uint32_t countDirection);
     
 uint32_t EncoderGetSpeedMode(int slaveAddress);
 
-void SetSpeedMode(int slaveAddress, uint32_t speedMode);
+void EncoderSetSpeedMode(int slaveAddress, uint32_t speedMode);
 
 uint32_t EncoderGetSpeedFilter(int slaveAddress);
 
-void SetSpeedFilter(int slaveAddress, uint32_t speedFilter);
+void EncoderSetSpeedFilter(int slaveAddress, uint32_t speedFilter);
 
 uint32_t EncoderGetLimitSwitchEnable(int slaveAddress);
 
-void SetLimitSwitchEnable(int slaveAddress, uint32_t limitSwitchEnable);
+void EncoderSetLimitSwitchEnable(int slaveAddress, uint32_t limitSwitchEnable);
 
 uint32_t EncoderGetLowLimitSwitch(int slaveAddress);
 
-void SetLowLimitSwitch(int slaveAddress, uint32_t lowLimitSwitch);
+void EncoderSetLowLimitSwitch(int slaveAddress, uint32_t lowLimitSwitch);
 
 uint32_t EncoderGetHighLimitSwitch(int slaveAddress);
 
-void SetHighLimitSwitch(int slaveAddress, uint32_t highLimitSwitch);
+void EncoderSetHighLimitSwitch(int slaveAddress, uint32_t highLimitSwitch);
 
 uint32_t EncoderGetDelay(int slaveAddress);
 
-void SetDelay(int slaveAddress, uint32_t delay);
+void EncoderSetDelay(int slaveAddress, uint32_t delay);
 
 uint32_t EncoderGetErrorReg(int slaveAddress);
 
-void SetErrorReg(int slaveAddress, uint32_t errorReg);
+void EncoderSetErrorReg(int slaveAddress, uint32_t errorReg);
 
-uint32_t EncoderGetDeviceResetStore(int slaveAddress);
+uint32_t EncoderGetDeviceReEncoderSetStore(int slaveAddress);
 
-void SetDeviceResetStore(int slaveAddress, uint32_t deviceResetStore);
+void EncoderSetDeviceReEncoderSetStore(int slaveAddress, uint32_t deviceReEncoderSetStore);
 
 uint32_t EncoderGetParameters(int slaveAddress);
 
-void SetParameters(int slaveAddress, uint32_t parameters);
+void EncoderSetParameters(int slaveAddress, uint32_t parameters);
     
 uint32_t EncoderGetAutoStore(int slaveAddress);
 
-void SetAutoStore(int slaveAddress, uint32_t autostore);
+void EncoderSetAutoStore(int slaveAddress, uint32_t autostore);
  
 uint32_t EncoderGetRestoreAllParameters(int slaveAddress);
 
-void SetRestoreAllParameters(int slaveAddress, uint32_t restoreAllParameters);
+void EncoderSetRestoreAllParameters(int slaveAddress, uint32_t restoreAllParameters);
     
 uint32_t EncoderGetRestoreAplicationParameters(int slaveAddress);
 
@@ -130,7 +130,7 @@ void SteRestoteAplicationParameters(int slaveAddress, uint32_t restoreAplication
         
 uint32_t EncoderGetAutoTest(int slaveAddress);
 
-void SetAutoTest(int slaveAddress, uint32_t autoTest);
+void EncoderSetAutoTest(int slaveAddress, uint32_t autoTest);
     
 uint32_t EncoderGetSoftwareVersion(int slaveAddress);
     
@@ -142,40 +142,36 @@ uint32_t EncoderGetRollCounter(int slaveAddress);
    
 uint32_t EncoderGetSerialParameters(int slaveAddress);
 
-void SetSerialParameters(int slaveAddress, uint32_t serialParameters);
+void EncoderSetSerialParameters(int slaveAddress, uint32_t serialParameters);
         
 uint32_t EncoderGetCommUpdate(int slaveAddress);
 
-void SetCommUpdate(int slaveAddress, uint32_t commUpdate);
+void EncoderSetCommUpdate(int slaveAddress, uint32_t commUpdate);
     
 uint32_t EncoderGetNodeAddress(int slaveAddress);
 
-void SetNodeAddress(int slaveAddress, uint32_t nodeAddress);
+void EncoderSetNodeAddress(int slaveAddress, uint32_t nodeAddress);
   
 uint32_t EncoderGetNodeUpdate(int slaveAddress);
 
-void SetNodeUpdate(int slaveAddress, uint32_t nodeUpdate);
+void EncoderSetNodeUpdate(int slaveAddress, uint32_t nodeUpdate);
     
 uint32_t EncoderGetAutoBaudEnable(int slaveAddress);
 
-void SetAutoBaudEnable(int slaveAddress, uint32_t autoBaudEnable);
+void EncoderSetAutoBaudEnable(int slaveAddress, uint32_t autoBaudEnable);
     
 uint32_t EncoderGetAutoBaudTimeout(int slaveAddress);
 
-void SetAutoBaudTimeout(int slaveAddress, uint32_t autoBaudTimeout);
+void EncoderSetAutoBaudTimeout(int slaveAddress, uint32_t autoBaudTimeout);
     
 uint32_t EncoderGetRestoreBusParameters(int slaveAddress);
 
-void SetRestoreBusParameters(int slaveAddress, uint32_t restoreBusParameters);
+void EncoderSetRestoreBusParameters(int slaveAddress, uint32_t restoreBusParameters);
   
 uint32_t EncoderGetTermination(int slaveAddress);
 
-void SetTermination(int slaveAddress, uint32_t termination);
+void EncoderSetTermination(int slaveAddress, uint32_t termination);
     
 uint32_t EncoderGetTermUpdate(int slaveAddress);
 
-void SetTermUpdate(int slaveAddress, uint32_t termUpdate);
-                                    
-        
-
-        
+void EncoderSetTermUpdate(int slaveAddress, uint32_t termUpdate);
