e0878fb1e91c5f173af94665a28dc138dfffba05
Bartosz Śmierciak <bart@bart-i5.home>
2021-06-10 18:19:11 +0200

    adding main file

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <modbus/modbus.h>
+#include "encoder.h"
+//#include "encoderRegisters.h"
+
+
+
+int main()
+{
+	EncoderInit("/dev/ttyUSB0", 19200 , 'N', 8, 1);
+	printf("Azymut [imp]: %d\n", EncoderGetPosition(127));
+	printf("Elewacja [imp]: %d\n", EncoderGetPosition(126));
+
+	EncoderClose();
+	return 0;
+}
